a364f38f997d6a43d5fd2adb1f02c2688b1c9ba6
Sérgio Adriano <sergioadrianobc@gmail.com>
2022-07-12 05:15:41 -0300

    bootcampSantander2022

---
+++ PROJETO_MODULO/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log("Azul", 36);
+console.log("Azul", "Brasília");
+console.log("Sérgio", "aprender")
---
+++ PROJETO_OOJS/script.js
@@ -0,0 +1,14 @@
+let lines = gets().split('\n');
+var N = parseInt(lines.shift());
+
+for (let i = 1; i <= N; i++) {
+
+//TODO: Complete os espaços em branco com uma possível solução para o desafio
+
+	if (i % 2 ===  0  ) 
+          print(`${i}^2 = ${ N**2}`);
+}
+
+
+
+let line = gets();
